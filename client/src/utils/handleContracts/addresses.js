6ed783cd29f22c6b037a5161a3b3d2125b70de8a
DanWizard <tortortorented@gmail.com>
2021-04-03 09:48:50 -0500

    addresses updated

--- client/src/utils/handleContracts/addresses.js
+++ client/src/utils/handleContracts/addresses.js
@@ -1,11 +1,11 @@
 const addresses = {
   kovan: {
-    stabilityPool: "0x5D0992f3F7Be325870bf0854392035f7CF35260B",
-    lusdToken: "0xc1a8a2b90b1f4eae4f46d3b9b31f3cd392dae377",
-    lqtyToken: "0xB2e3FE46688Dc9aC3a99983066d057E461cbD887",
+    stabilityPool: "0x04d630Bff6dea193Fd644dEcfC460db249854a02",
+    lusdToken: "0x0b02b94638daa719290b5214825da625af08a02f",
+    lqtyToken: "0xd8FABEa5752D52Ee0dEbF6c90948F5446694EE7c",
     ethLPToken: "0xfb91e5d2bcafcc336a2c5c0f0cf6749b0721ea12",
     lusdLPToken: "0x5036882451a000407ade3f9f735ba523f8c9f209",
-    lqtyStaking: "0xdb456329498CBDbb3Fa63f9Bc2792088ce8b933A",
+    lqtyStaking: "0x82b7384e360DD3f16ed23D15921BB7a878a08b87",
   },
 };
 
